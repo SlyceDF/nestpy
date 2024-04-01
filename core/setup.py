1fdd47ba8920034530bb571eb882f5037302f94c
the real Slyce @ pc <91631241+SlyceDF@users.noreply.github.com>
2024-04-01 20:32:58 +0200

    0.3.2 fixed

--- core/setup.py
+++ core/setup.py
@@ -1,27 +1,35 @@
 from setuptools import find_packages, setup
 from os import path as osp, system as runcmd
 
+def parse(filename):
+    return osp.join(osp.dirname(__file__), filename)
+
 def read(filename):
-    return open(osp.join(osp.dirname(__file__), filename)).read()
+    return open(parse(filename), 'r').read()
 
 
 param = eval(read('param.i'))
 
 version = param['version']
 test = param['test']
-setup(
-    name='nestpython',
-    packages=find_packages(include=['nestpy']),
-    version=version,
-    description='python with braces.',
-    author='slycedf',
-    license='MIT',
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        "Development Status :: 4 - Beta"
-    ]
-)
+
+with open(parse('../README.md'), 'r') as f, open(parse('README.md'), 'w') as fn:
+    readme = f.read()
+    fn.write(readme)
+
+    setup(
+        name='nestpython',
+        packages=find_packages(include=['nestpy']),
+        version=version,
+        description='python with braces.',
+        author='slycedf',
+        license='MIT',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        classifiers=[
+            "Development Status :: 4 - Beta"
+        ]
+    )
 
 token = open(f'D:/slycefolder/ins/nsp/{ {True: "tt", False: "tr"}[test]}', 'r').read()
 
