316c328d9806c52633b802aa846bf229eafb84bd
the real Slyce @ pc <91631241+SlyceDF@users.noreply.github.com>
2024-04-06 18:24:07 +0200

    0.3.9: fix recursive building

--- core/nestpy/files.py
+++ core/nestpy/files.py
@@ -4,18 +4,35 @@ from os import mkdir as _mkdir
 from os import path as _path
 from os import remove as _remove
 from os import walk as _walk
+from os import scandir as _scandir
+from os import chdir as _chdir
+from os import getcwd as _getcwd
 from shutil import copyfile as _copyfile
 from shutil import rmtree as _rmtree
 
 from . import main as _m
 
-slashConverter = str.maketrans('\\','/')
+_slashConverter = str.maketrans('\\','/')
 def _getAllFilePaths(dirPath):
     return [
-        _path.join(dirpath,f).translate(slashConverter) for (
+        _path.join(dirpath,f).translate(_slashConverter) for (
             dirpath, dirnames, filenames
         ) in _walk(dirPath) for f in filenames
     ]
+
+def _getFilesDirs(dirPath):
+    oldDir=_getcwd()
+    _chdir(dirPath)
+    a = []
+    b = []
+    for f in _scandir():
+        g = f.path.translate(_slashConverter).removeprefix('./')
+        if f.is_dir():
+            b.append(g)
+        else:
+            a.append(g)
+    _chdir(oldDir)
+    return a, b
 
 def _filterByFileExt(files, fileExt):
     passed = []
@@ -44,29 +61,48 @@ def ncompile_to(file, new_file=None, indent_amount=1, replace_previous=False):
     if i.lower() == 'y':
       compile(file)
 
-def nbuild(dir, new_dir, indent_amount=1, erase_dir=False, 
+
+def nbuild(dir, new_dir, indent_amount=1, erase_dir=None,
            replace_previous=False, transfer_other_files=True):
-    if _path.isdir(new_dir):
+ subpath = ''
+ def subbuild():
+    nonlocal dir
+    nonlocal subpath
+    nonlocal new_dir
+    nonlocal indent_amount
+    nonlocal erase_dir
+    nonlocal replace_previous
+    nonlocal transfer_other_files
+    print(subpath)
+    if _path.isdir(f'{new_dir}/{subpath}'):
       def remove():
-        _rmtree(new_dir)
-        _mkdir(new_dir)
-      if erase_dir:
-        remove()
-      else:
-        i = input(
-          f'Directory \'{new_dir}\' already exists. Would you like to erase it? [y/(n)]: ')
-        if i.lower() == 'y':
+        _rmtree(f'{new_dir}/{subpath}')
+        _mkdir(f'{new_dir}/{subpath}')
+      match erase_dir:
+        case True:
           remove()
+        case None:
+          i = input(
+            f'Directory \'{new_dir}/{subpath}\' already exists. Would you like to erase it? [y/(n)]: ')
+          if i.lower() == 'y':
+            remove()
+
     else:
-      _mkdir(new_dir)
-    compilable, leaveBe = _filterByFileExt(_getAllFilePaths(dir), '.npy')
+      _mkdir(f'{new_dir}/{subpath}')
+    compilable, leaveBe = _filterByFileExt(_getFilesDirs(f'{dir}/{subpath}')[0], '.npy')
+    print(_getFilesDirs(f'{dir}/{subpath}'))
     for file in compilable:
-      ncompile_to(file, f'{new_dir}/{file.split("/")[-1].rsplit(".", 1)[0]}.py', 
+      ncompile_to(f'{dir}/{subpath}/{file}', f'{new_dir}/{subpath}/{file.rsplit("/", 1)[-1].rsplit(".", 1)[0]}.py',
                indent_amount, replace_previous)
     if transfer_other_files:
       for file in leaveBe:
-        print(f'> transferring {file}')
-        _copyfile(file, f'{new_dir}/{file.split("/")[-1]}')
+        print(f'> transferring {dir}/{subpath}/{file}')
+        _copyfile(f'{dir}/{subpath}/{file}', f'{new_dir}/{subpath}/{file.rsplit("/", 1)[-1].lstrip("/")}')
+    for subdir in _getFilesDirs(f'{dir}/{subpath}')[~0]:
+        subpath += subdir
+        subbuild()
+
+ subbuild()
 
 def ncompile(file, indent_amount=1):
   with open(file, 'r') as f:
--- core/param.i
+++ core/param.i
@@ -1,1 +1,1 @@
-{'test': False, 'version': '0.3.8'}
+{'test': False, 'version': '0.3.9'}
